afb002c5af0fe56ba1416786ed57a1d9ef0d514c
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-10-23 23:44:16 +0600

    Class Details

--- src/assets/Pages/Class/ClassDetails.jsx
+++ src/assets/Pages/Class/ClassDetails.jsx
@@ -5,6 +5,7 @@ import { AiFillStar } from "react-icons/ai";
 import { RxEnter } from "react-icons/rx";
 import { FiCheck, FiX } from "react-icons/fi";
 import SuggestedClass from '../SuggestedClass/SuggestedClass';
+import TrainingReview from '../TrainingReview/TrainingReview';
 
 
 const ClassDetails = () => {
@@ -14,9 +15,12 @@ const ClassDetails = () => {
 
     const travelInformation = [
 
+
+
+
         { label: 'Course Type', value: ['Training'] },
-        { label: 'Included', value: ['✓ Included Services 1', '✓ Included Services 2', '✓ Included Services 3', '✓ Included Services 4', '✓ Included Services 5'] },
-        { label: 'Not Included', value: ['✕ Not Included Services 1', '✕ Not Included Services 2', '✕ Not Included Services 3', '✕ Not Included Services 3'] }
+        { label: 'Included', value: ['✓ Certificate', '✓ Competition Opportunities', '✓ Learning Resources', '✓ Practical Sessions', '✓ Personal Support', '✓ Career Counseling'] },
+        { label: 'Not Included', value: ['✕ Sports Equipment', '✕ Clothing and Footwear', '✕ Personal Accessories', '✕ Tournament Entry Fees', '✕ Accommodation', '✕ Meals'] }
     ];
 
     const handleTabClick = (tabNumber) => {
@@ -31,43 +35,28 @@ const ClassDetails = () => {
 
     return (
         <div className='mx-auto max-w-7xl'>
-            {/* <div className="h-[150px] sm:h-[170px] lg:h-[200px] xl:h-[250px] bg-cyan-50 flex flex-col items-center justify-center "> */}
-            <div className="h-[150px] sm:h-[170px] lg:h-[200px] xl:h-[250px] relative flex flex-col items-center justify-center bg-cover bg-center mb-5 bg-blend-darken bg-[url('https://i.ibb.co/hMFqR2m/Data.jpg')]">
-                <div className="absolute w-full bg-black opacity-90"></div>
-                <div className="z-10 p-5 mx-auto text-center sm:w-2/3 sm:pl-10">
-                    <h2 className="text-5xl font-bold text-black sm:text-3xl ">
-                        Class Pages
-                    </h2>
-                </div>
-            </div>
             <div className='grid grid-cols-12 gap-4 mx-auto max-w-7xl md:grid-cols-12'>
                 {classData ? (
                     <>
                         {/* Left Side - Class Information */}
-                        <div className="col-span-12 border-2 md:col-span-8 class-image">
-                            <h2 className='text-xl font-bold text-center text-cyan-500'>{classData?.className}</h2>
-                            {/* <img className='w-1/2 p-2 mx-auto border-2 rounded lg:w-2/3 border-cyan-500 shadowAlll' src={classData?.classImg} alt="Instructor" />
-                            <h2 className='text-xl font-bold text-center text-cyan-500'>{classData?.className}</h2>
-                            <div className='text-center'>
-                                <p>Instructor: {classData?.instructorName}</p>
-                                <p>Available Seats: {classData?.availableSeats}</p>
-                                <p>Price: ${classData?.price}</p>
-                            </div> */}
+                        <div className="col-span-12 mt-5 md:col-span-8 class-image">
+                            <h2 className='text-2xl font-semibold text-center md:text-3xl text-cyan-500'>{classData?.className}</h2>
+
                             <div>
                                 <div className="flex my-5 space-x-2">
                                     <button
                                         onClick={() => handleTabClick(1)}
-                                        className={`py-2 px-4 rounded ${activeTab === 1 ? 'bg-cyan-500 text-white' : 'bg-gray-300 text-gray-700'
+                                        className={`ml-4 py-1 px-4 rounded ${activeTab === 1 ? 'bg-cyan-500 text-white' : 'bg-gray-300 text-gray-700'
                                             }`}
                                     >
-                                        Overview
+                                        Course Overview
                                     </button>
                                     <button
                                         onClick={() => handleTabClick(2)}
-                                        className={`py-2 px-4 rounded ${activeTab === 2 ? 'bg-cyan-500 text-white' : 'bg-gray-300 text-gray-700'
+                                        className={`ml-4 py-1 px-4 rounded ${activeTab === 2 ? 'bg-cyan-500 text-white' : 'bg-gray-300 text-gray-700'
                                             }`}
                                     >
-                                        Curriculum
+                                        Content & Review
                                     </button>
                                     {/* <button
                                         onClick={() => handleTabClick(3)}
@@ -76,13 +65,13 @@ const ClassDetails = () => {
                                     >
                                         Course Instructor
                                     </button> */}
-                                    <button
+                                    {/* <button
                                         onClick={() => handleTabClick(4)}
                                         className={`py-2 px-4 rounded ${activeTab === 4 ? 'bg-cyan-500 text-white' : 'bg-gray-300 text-gray-700'
                                             }`}
                                     >
                                         Review
-                                    </button>
+                                    </button> */}
                                 </div>
 
                                 <div>
@@ -95,7 +84,7 @@ const ClassDetails = () => {
                                             </div>
                                             <div className='my-5'>
                                                 <p className='mb-2 text-xl font-semibold md:text-2xl text-cyan-500'>What Will I Learn From This Training?</p>
-                                                <h1 className='mb-2 text-justify'>
+                                                <h1 className='mb-3 text-justify'>
                                                     Maximus Ligula Eleifend Id Nisl Quis Interdum. Sed Malesuada Tortor Non Turpis Semper Bibendum. </h1>
                                                 <div className="border-gray-100">
 
@@ -105,11 +94,11 @@ const ClassDetails = () => {
                                                             key={label}
                                                         >
                                                             <div className="w-full text-start sm:w-2/5 ">
-                                                                <h3 className="text-sm font-bold">{label}</h3>
+                                                                <h3 className="text-sm font-semibold">{label}</h3>
                                                             </div>
                                                             <div className="flex flex-col w-full sm:justify-start sm:items-start sm:flex-wrap sm:flex-row md:w-3/5">
                                                                 {value.map((item, index) => (
-                                                                    <p className="ml-0 text-gray-600 sm:ml-10" key={index}>
+                                                                    <p className="ml-0 text-gray-800 sm:ml-10" key={index}>
                                                                         {item}
                                                                         {index !== value.length - 1 && ', '}
                                                                     </p>
@@ -133,7 +122,7 @@ const ClassDetails = () => {
                                                 <table className="min-w-full p-1 border bg-slate-300 border-slate-400">
                                                     <thead>
                                                         <tr>
-                                                            <th className="py-2 font-medium tracking-wider text-center text-gray-900 uppercase border sm:px-6 sm:py-3 text-md border-slate-400">Syllabus</th>
+                                                            <th className="py-2 font-medium tracking-wider text-center text-gray-900 uppercase border sm:px-6 sm:py-3 text-md border-slate-400">Training</th>
                                                             <th className="py-2 font-medium tracking-wider text-center text-gray-900 uppercase border sm:px-6 sm:py-3 text-md border-slate-400">Course Content</th>
                                                         </tr>
                                                     </thead>
@@ -145,10 +134,17 @@ const ClassDetails = () => {
                                                             </tr>
                                                         ))}
                                                     </tbody>
+
+
                                                 </table>
+
                                             ) : (
                                                 <p>No course content available.</p>
                                             )}
+                                            <div className='my-10'>
+                                                <h1 className='text-2xl font-semibold text-center text-cyan-500'>Training Review</h1>
+                                                <TrainingReview></TrainingReview>
+                                            </div>
                                         </>
                                     )}
                                     {/* {activeTab === 3 && (
@@ -157,12 +153,12 @@ const ClassDetails = () => {
                                             <p>Course Instructor content goes here.</p>
                                         </>
                                     )} */}
-                                    {activeTab === 4 && (
+                                    {/* {activeTab === 4 && (
                                         <>
                                             <h2>Review</h2>
                                             <p>Review content goes here.</p>
                                         </>
-                                    )}
+                                    )} */}
                                 </div>
                             </div>
                             {/* <img className='w-1/2 p-2 mx-auto border-2 rounded lg:w-2/3 border-cyan-500 shadowAlll' src={classData?.classImg} alt="Instructor" />
@@ -176,7 +172,7 @@ const ClassDetails = () => {
                         </div>
 
                         {/* Right Side - Teacher Information */}
-                        <div className="w-full col-span-12 mx-auto border-2 shadow-xl md:col-span-4 teacher-info">
+                        <div className="w-full col-span-12 mx-auto sideShadow md:col-span-4 teacher-info">
                             <img className='w-full p-3 mx-auto rounded-md' src={classData?.classImg} alt="Instructor" />
 
                             <div className='flex items-center justify-between px-4 text-lg font-semibold'>
